08ec8e9a719d6fba2fbe4a72baa88cde0de69c62
quilper <o.pilker@gmail.com>
2019-04-28 17:49:34 -0300

    salvando dados no playerPrefs

---
+++ Assets/Scripts/ui/GuruData.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+public class GuruData {
+	public int Type { get; set; }
+	public string Name { get; set; }
+	public string CultName { get; set; }
+	public string HollyBookName { get; set; }
+}
+	// private static string cultName, bookName, guruName;
+	// private static int guruIndex;
+
+	// public GuruData(string name)
+	// {
+	// 	GuruData.GuruName = name;
+	// }
+
+	// public static string GuruName
+	// {
+	// 	get
+	// 	{
+	// 		return guruName;
+	// 	}
+	// 	set
+	// 	{
+	// 		guruName = value;
+	// 	}
+	// }
+
+	// public static string BookName
+	// {
+	// 	get
+	// 	{
+	// 		return bookName;
+	// 	}
+	// 	set
+	// 	{
+	// 		bookName = value;
+	// 	}
+	// }
+
+	// public static string CultName
+	// {
+	// 	get
+	// 	{
+	// 		return cultName;
+	// 	}
+	// 	set
+	// 	{
+	// 		cultName = value;
+	// 	}
+	// }
+
+	// public static int GuruIndex
+	// {
+	// 	get
+	// 	{
+	// 		return guruIndex;
+	// 	}
+	// 	set
+	// 	{
+	// 		guruIndex = value;
+	// 	}
+	// }
---
+++ Assets/Scripts/ui/UI_Screen_Setup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Screen_Setup : UI_Screen
+{
+	#region Variables
+	[Header("Setup Cult Properties")]
+	public InputField cultNameInput;
+	public InputField hollyBookNameInput;
+	public InputField guruNameInput;
+	// public GuruData guru { get; private set; }
+
+	static string guruName;
+
+	#endregion
+
+	#region Helpers
+	public virtual void SetupGameOptions()
+	{
+
+		PlayerPrefs.SetInt("GuruType", 0);
+		if (guruNameInput.text != null || guruNameInput.text != "")
+		{
+			PlayerPrefs.SetString("GuruName", guruNameInput.text);
+		}
+		if (hollyBookNameInput.text != null || hollyBookNameInput.text != "")
+		{
+			PlayerPrefs.SetString("HollyBookName", hollyBookNameInput.text);
+		}
+		if (cultNameInput.text != null || cultNameInput.text != "")
+		{
+			PlayerPrefs.SetString("CultName", cultNameInput.text);
+		}
+	}
+	#endregion
+}
