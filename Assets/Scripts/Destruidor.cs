0ea4d5febdf12126ebf365447e44746ab2901ab7
Simon <simon.ducroquet@gmail.com>
2019-04-29 01:18:51 -0300

    som moedinha

--- Assets/Scripts/Destruidor.cs
+++ Assets/Scripts/Destruidor.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Destruidor : MonoBehaviour
 {
 	public GameObject objectWithOtherScript;
+	AudioSource audio;
+
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		objectWithOtherScript.GetComponent<DataController>().addFollowers(1);
+		audio.Play(0);
 		Destroy(other.gameObject);
 	}
 }
